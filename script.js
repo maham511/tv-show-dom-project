b780f645389b6b6127a6b5b38592147200aa7d35
maham511 <maha.malik511@gmail.com>
2021-09-09 12:40:09 +0100

    Css updated

--- script.js
+++ script.js
@@ -6,26 +6,17 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
 
-//create ul and append to body
-//render one episode
-//loop through all episodes and render names of all 73
-
-//Bugs
-//<p></p> shows in summary
-  //--Create a div instead of p?
-//season and ep nums need padding and in same line
-
+//wrapper div
 const wrapDiv = document.createElement('div');
 document.body.appendChild(wrapDiv);
 wrapDiv.id = 'wrapper';
-// wrapDiv.appendChild(epDiv);
 
-
+//creates & appends div, h2, img, p & a element for each episode
 getAllEpisodes().forEach((episode) => {
   let epDiv = document.createElement("div");
   wrapDiv.appendChild(epDiv);
@@ -39,13 +30,6 @@ getAllEpisodes().forEach((episode) => {
   epName.innerText = `${episode.name} - S${episode.season
     .toString()
     .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`; //season & ep num changed to string to add zero-padding
-
-  // //Season & Episode Number
-  // let seasonEpisode = document.createElement("h2");
-  // epDiv.appendChild(seasonEpisode);
-  // seasonEpisode.innerText = `S${episode.season
-  //   .toString()
-  //   .padStart(2, "0")} E${episode.number.toString().padStart(2, "0")}`; //season no. changed to string to add padding, then rendered as h2 elem
 
   //Medium Image
   let imgMed = document.createElement("img");
@@ -63,16 +47,9 @@ getAllEpisodes().forEach((episode) => {
   epLink.innerHTML = `<a href= ${episode.url} target="_blank" rel="noopener noreferrer">Original episode info</a>`;
 });
 
-// const wrapDiv = document.createElement("div");
-// document.body.appendChild(wrapDiv);
-// wrapDiv.appendChild(".episode");
-// wrapDiv.appendChild(epDiv);
-
-
 //Link to TV Maze
  let tvMazeLink = document.createElement("p");
  document.body.appendChild(tvMazeLink);
  tvMazeLink.innerHTML =
    'All data from <a href="https://tvmaze.com/" target="_blank" rel="noopener noreferrer">TVMaze.com</a>';
 
-// wrapDiv.appendChild(".episode");
