65dec7122bd896c8ca4475a394ddeb919f4c9e21
maham511 <maha.malik511@gmail.com>
2021-09-16 23:38:21 +0100

    Main files refactored, Css added till level 300

--- script.js
+++ script.js
@@ -1,111 +1,84 @@
 //You can edit ALL of the code here
 
-//global variable
+//global variables
 const allEpisodes = getAllEpisodes();
 
 function setup() {
-  // const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes); //calls makePageForEpisodes function using allEpisodes var as parameter
+  //makes cards for all episodes
+  makePageForEpisodes(allEpisodes);
 }
 
-//function creates cards for each episode
+// creates cards for given episodes list
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+  //element empty onload
   rootElem.innerHTML = "";
 
-  //wrapper div
-  // const wrapDiv = document.createElement("div");
-  // rootElem.appendChild(wrapDiv);
-  // wrapDiv.id = "wrapper";
-
-  //creates & appends div, h2, img, p & a element for each episode
+  //creates & appends div, h2, img, p & a elements for each episode
   episodeList.forEach((episode) => {
-    let epDiv = document.createElement("div");
-    rootElem.appendChild(epDiv);
-    epDiv.className = "episode";
+    //Episode card
+    let epCard = document.createElement("div");
+    epCard.className = "epCard";
+    rootElem.appendChild(epCard);
 
-    //Episode Name, Season & Ep number
-    let epName;
-    epName = document.createElement("h2");
-    epDiv.appendChild(epName);
+    //Episode Name, Season & Number
+    let epHeading = document.createElement("h2");
+    let epName = episode.name;
+    let epSeason = episode.season.toString().padStart(2, "0");
+    let epNumber = episode.number.toString().padStart(2, "0");
 
-    //innertext renders ep name, season & ep number
-    epName.innerText = `${episode.name} - S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`; //season & ep num changed to string to add zero-padding
+    epHeading.innerText = `${epName} - S${epSeason}E${epNumber}`;
 
     //Medium Image
-    let imgMed = document.createElement("img");
-    epDiv.appendChild(imgMed);
-    imgMed.src = episode.image.medium; //renders medium img
+    let epImg = document.createElement("img");
+    epImg.src = episode.image.medium;
 
     //Summary
     let epSummary = document.createElement("p");
-    epDiv.appendChild(epSummary);
-    epSummary.innerHTML = episode.summary; //innerHTML removes all tags in summary string
+    epSummary.className = "summary";
+    epSummary.innerHTML = episode.summary; //innerHTML removes all p tags in summary obj property
 
     //Episode Link
-    let epLink = document.createElement("a");
-    epDiv.appendChild(epLink);
-    epLink.innerHTML = `<a href= ${episode.url} target="_blank" rel="noopener noreferrer">Original episode info</a>`;
+    let epLink = document.createElement("p");
+    epLink.innerHTML = `<a href= ${episode.url} target="_blank" rel="noopener noreferrer">Original episode</a>`;
+
+    //Child elems appended to epCard
+    epCard.append(epHeading, epImg, epSummary, epLink);
+
+    //Select menu options created and appended for all eps
+    let option = document.createElement("option");
+    selectMenu.appendChild(option);
+    option.innerText = `S${epSeason}E${epNumber} - ${episode.name}`;
   });
 }
 
-//Level 200
-//Add search
-//needs a 'live' search input
-//1. only display eps whose summary OR name match search term
-//2. be case insensitive
-//3. Update display asap after each keystroke
-//4. Display how many episodes match search. eg. displaying 12/73 episodes
-//5. If search empty, show all eps
-
+// Level 200 - Searchbar
 let searchBar = document.getElementById("searchbar");
 let displayNumber = document.getElementById("display-number"); //p elem
 
 searchBar.addEventListener("input", searchFilter);
 
 function searchFilter(event) {
-  let userSearch = event.target.value.toLowerCase();
+  //toLowerCase makes search case-insensitive
+  let searchInput = event.target.value.toLowerCase();
+
+  //filters for eps with matching name or summary
   let filteredEpisodes = allEpisodes.filter((episode) => {
     return (
-      episode.name.toLowerCase().includes(userSearch) ||
-      episode.summary.toLowerCase().includes(userSearch)
+      episode.name.toLowerCase().includes(searchInput) ||
+      episode.summary.toLowerCase().includes(searchInput)
     );
   });
 
-  //creates filtered episodes' cards
+  //creates filtered cards
   makePageForEpisodes(filteredEpisodes);
 
-  //eg displaying 10/73 episodes
+  //updates para showing number of matching episodes
   displayNumber.innerText = `Showing ${filteredEpisodes.length}/${allEpisodes.length} episodes`;
 }
 
-//-------------------------------------
-//LEVEL 300
-
-//Selectmenu stored
-let selectMenu = document.getElementById("select-episode");
-
-//add all eps to drop down options
-allEpisodes.forEach((episode) => {
-  let option = document.createElement("option");
-  selectMenu.appendChild(option);
-  // option.value = `S${episode.season
-  //     .toString()
-  //     .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-  option.innerText = `S${episode.season
-    .toString()
-    .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
-    episode.name
-  }`;
-  //  console.log(option.value); //logs each episode's innerText value
-});
-
-//only show selected episode
-//Add eventlistenter for select elem
-//use 'change'
-//test if can update p elem on change
+//LEVEL 300 - Select Menu
+let selectMenu = document.getElementById("selectMenu");
 selectMenu.addEventListener("change", showSelectedEpisode);
 
 function showSelectedEpisode(event) {
@@ -115,16 +88,13 @@ function showSelectedEpisode(event) {
     return event.target.value.includes(episode.name);
   });
 
-  //shows selected episode
-  makePageForEpisodes(selectedEpisode);
+  if (selectMenu.value === "All episodes") {
+    makePageForEpisodes(allEpisodes);
+  } else {
+    makePageForEpisodes(selectedEpisode);
+  }
   //clears displayNumber text when user selects any option
   displayNumber.innerText = "";
-
-  if (selectMenu.value === "All episodes") {
-    // console.log("ye"); //logs ye
-    makePageForEpisodes(allEpisodes);
-    // displayNumber.innerText = "";
-  }
 }
 
 window.onload = setup;
